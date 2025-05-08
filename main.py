574b1ec54a56702bfd81d5b31f66b53e1d961f5c
clyde (vadim khakhanov) <134577210+vadiohead@users.noreply.github.com>
2025-05-08 01:45:53 +0000

    HW 5

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///songs.db'
 
 db = SQLAlchemy(app)
@@ -82,6 +81,8 @@ def fortune():
             </form>
         """
 
+# databases
+
 class SongList(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String())
@@ -95,24 +96,24 @@ class SongList(db.Model):
         self.year = year
         self.video = video
 
-def populate_db():
-    db.create_all()
-    songs = [
-            SongList(title='Ice Cube - My Summer Vacation', album='Death Certificate', year=1991,
-                     video='https://youtube.com/watch?v=SXrWIyCW-7E'),
-            SongList(title='Car Seat Headrest - Gethsemane', album='The Scholars', year=2025,
-                     video='https://youtu.be/RAGA2fmBSJo?si=FtCJZZyGhYmnvUcn'),
-            SongList(title='The Beloved - Sweet Harmony', album='Conscience', year=1993,
-                     video='https://youtu.be/rP9Z5Pc8cRM?si=1nSsg7XkPkwBNPIo'),
-            SongList(title='Yeule - sulky baby', album='softscars', year=2023,
-                     video='https://youtu.be/ca7zUSNpL70?si=Yo9sk9hmVYip0pCD'),
-            SongList(title='Noize MC - Ругань из-за стены', album='Последний альбом', year=2011,
-                     video='https://youtu.be/vr1qkx3hfx8?si=kGrhs0Dy_mMRPykZ'),
-            SongList(title='Alihan Dze - Бухы дээрэ (ft. Saryuna)', album='Шата', year=2016,
-                     video='https://youtu.be/vP3oc-pHRtE?si=AmdcESSAb4hU2ojZ')
-        ]
-    db.session.bulk_save_objects(songs)
-    db.session.commit()
+#def populate_db():
+    #db.create_all()
+    #songs = [
+    #        SongList(title='Ice Cube - My Summer Vacation', album='Death Certificate', year=1991,
+    #                 video='https://youtube.com/watch?v=SXrWIyCW-7E'),
+    #        SongList(title='Car Seat Headrest - Gethsemane', album='The Scholars', year=2025,
+    #                 video='https://youtu.be/RAGA2fmBSJo?si=FtCJZZyGhYmnvUcn'),
+    #        SongList(title='The Beloved - Sweet Harmony', album='Conscience', year=1993,
+    #                 video='https://youtu.be/rP9Z5Pc8cRM?si=1nSsg7XkPkwBNPIo'),
+    #       SongList(title='Yeule - sulky baby', album='softscars', year=2023,
+    #                 video='https://youtu.be/ca7zUSNpL70?si=Yo9sk9hmVYip0pCD'),
+    #        SongList(title='Noize MC - Ругань из-за стены', album='Последний альбом', year=2010,
+    #                 video='https://youtu.be/vr1qkx3hfx8?si=kGrhs0Dy_mMRPykZ'),
+    #        SongList(title='Alihan Dze - Бухы дээрэ (ft. Saryuna)', album='Шата', year=2016,
+    #                 video='https://youtu.be/vP3oc-pHRtE?si=AmdcESSAb4hU2ojZ')
+    #]
+    #db.session.bulk_save_objects(songs)
+    #db.session.commit()
 
 @app.route('/songs')
 def show_songs():
@@ -121,7 +122,48 @@ def show_songs():
     return f"""
         <h2>Song list</h2>
         <ul>{song_list}</ul>
+        <a href="/songs/add">Add a New Song</a><br><br>
         <a href="/">Main Page</a>
+    """
+
+@app.route('/songs/add', methods=['GET', 'POST'])
+def add_song():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        album = request.form.get('album')
+        year = request.form.get('year')
+        video = request.form.get('video')
+        
+        if not title or not album or not year or not video:
+            return """
+                <p>All fields are required!</p>
+                <a href="/songs/add">Go Back</a>
+            """
+
+        try:
+            year = int(year)
+        except ValueError:
+            return """
+                <p>Year must be a valid number!</p>
+                <a href="/songs/add">Go Back</a>
+            """
+
+        new_song = SongList(title=title, album=album, year=year, video=video)
+        db.session.add(new_song)
+        db.session.commit()
+        return redirect(url_for('show_songs'))
+
+    return """
+        <h2>Add a New Song</h2>
+        <form method="POST">
+            Title:           <input type="text" name="title"><br><br>
+            Album:           <input type="text" name="album"><br><br>
+            Year:            <input type="text" name="year"><br><br>
+            Music Video URL: <input type="text" name="video"><br><br>
+            <input type="submit" value="Add Song">
+        </form>
+        <br>
+        <a href="/songs">Back to Songs List</a>
     """
 
 @app.route('/songs/<int:song_id>')
@@ -137,5 +179,13 @@ def song_detail(song_id):
         <a href="/songs">Back to Songs List</a>
     """
 
+@app.route('/clear-db')
+def clear_songs():
+    SongList.query.delete()
+    db.session.commit()
+    return "<p>Database has been cleared.</p><a href='/'>Go to Main Menu</a>"
+
 if __name__ == '__main__':
+    #with app.app_context():
+    #    populate_db()
     app.run(debug=True, host='0.0.0.0')
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 {pkgs}: {
   deps = [
+    pkgs.sqlite
     pkgs.imagemagick_light
     pkgs.mailutils
     pkgs.lacus
