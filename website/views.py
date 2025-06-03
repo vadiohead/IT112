c068ba1fc129df8da5ce10b42318b3aaecc3e744
vadiohead <ehutyuh@gmail.com>
2025-06-02 23:37:18 -0700

    HW 9 + deployment fix

--- website/views.py
+++ website/views.py
@@ -14,57 +14,30 @@ def about(request):
     """)
 
 def fortune(request):
-    colors = ['chartreuse', 'turquoise', "elephant's breath", 'red']
-    numbers = ['1', '2']
+    fortunes = {
+        ('chartreuse', '1'): "you are lucky",
+        ('chartreuse', '2'): "peace upon you",
+        ('turquoise', '1'): "fill you with wit",
+        ('turquoise', '2'): "do not pick elephant's breath",
+        ("elephant's breath", '1'): "phoque yew",
+        ("elephant's breath", '2'): "straight outta compton",
+        ('red', '1'): "you will find something surprising",
+        ('red', '2'): "you will do something amazing"
+    }
 
     if request.method == 'POST':
         name = request.POST.get('user', 'Mysterious guest')
         color = request.POST.get('color')
         number = request.POST.get('number')
-
-        fortunes = {
-            ('chartreuse', '1'): "you are lucky",
-            ('chartreuse', '2'): "peace upon you",
-            ('turquoise', '1'): "fill you with wit",
-            ('turquoise', '2'): "do not pick elephant's breath",
-            ("elephant's breath", '1'): "phoque yew",
-            ("elephant's breath", '2'): "straight outta compton",
-            ('red', '1'): "you will find something surprising",
-            ('red', '2'): "you will do something amazing"
-        }
         fortune_msg = fortunes.get((color, number), "0xc0ffee: undefined")
+        return render(request, 'fortune_result.html', {
+            'name': name,
+            'fortune_msg': fortune_msg
+        })
 
-        return HttpResponse(f"""
-            <p>{name}, your fortune is:<strong> {fortune_msg}</strong></p>
-            <a href="/fortune">Try Again</a>
-        """)
+    return render(request, 'fortune.html')
 
-    else:
-        return HttpResponse("""
-            <h2>Get Your Fortune</h2>
-            <form method="POST" action="/fortune">
-                <label>Your name: </label><br>
-                <input type="text" name="user"><br><br>
-
-                <label>Choose a color:</label><br>
-                <select name="color">
-                    <option>chartreuse</option>
-                    <option>turquoise</option>
-                    <option>elephant's breath</option>
-                    <option>red</option>
-                </select><br><br>
-
-                <label>Choose a number:</label><br>
-                <select name="number">
-                    <option>1</option>
-                    <option>2</option>
-                </select><br><br>
-
-                <button type="submit">Submit</button>
-                <br>
-                <a href="/">Main Page</a>
-            </form>
-        """)
+# databases
 
 def show_songs(request):
     songs = SongList.objects.all()
