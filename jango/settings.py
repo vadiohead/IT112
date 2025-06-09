6d315d55f45ef3be92b985ce0db1cfcbf6136e8a
vadiohead <ehutyuh@gmail.com>
2025-06-09 14:13:34 -0700

    HW 10 + deployment fixes

--- jango/settings.py
+++ jango/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- website/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SongList
-
-class SongSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SongList
-        fields = ['id', 'title', 'album', 'year', 'video']
